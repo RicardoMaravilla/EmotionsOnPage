fbadc0f8148b1a13b6fdb0a53aed820a80976b8e
Ricardo Maravilla <ricardo.madelga@gmail.com>
2021-11-30 20:54:33 -0600

    Login
    Login Funcionando

--- users/views.py
+++ users/views.py
@@ -87,15 +87,12 @@ def show_privacy(request):
 def show_test(request):
 	return render(request, "test.html")
 
-<<<<<<< HEAD
 def show_emociones(request):
 	return render(request, "emociones.html")
 
 def show_Login(request):
 	return render(request,"Login.html")
 	
-=======
->>>>>>> main
 # Agregar el cambio de password
 def change_psswd(request):
     if request.method == 'POST':
